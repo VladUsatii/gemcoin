dc2a39cfa64c6547c62190bcb175d2761c92fcdc
Vlad Usatii <vladusatii@gmail.com>
2021-12-21 19:13:10 -0600

    trying to set up a p2p network with plugins first

---
+++ gemcoin/peers/main.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import socket
+import sys, os
+import time
+import asyncio
+import random
+
+from p2pLib import srcNode
+
+IP = socket.gethostbyname(socket.gethostname())
+
+# diffie-hellman constants
+DH_GEN = 9
+DH_MOD = 37
+
+def localAddresses():
+	c1, c2 = '(', ')'
+	IPs = []
+
+	usableIPs = []
+
+	for device in os.popen('arp -a'): IPs.append(device)
+	for index, x in enumerate(IPs):
+		IPs[index] = x[x.find(c1)+1: x.find(c2)]
+	for index, x in enumerate(IPs):
+		if x[-2:] != ".0" and x[-4:] != ".255":
+			usableIPs.append(x)
+
+	return usableIPs
+
+"""
+Peer discovery packet:
+
+Concatenates privateKey, diffie-hellman number, and sessionKey; pings host with data, host decodes diffie-hellman number, uses it for key. AES is then allowed to be used.
+"""
+def discoveryPacket():
+	# privateKey | DHKE1_num | sessionKey
+	sessionKey = time.strftime("%m-%d-%Y-%H")
+	privateKey = "future-key"
+	DHKE1_num = (DH_GEN**(random.random()*1000)) % DH_MOD
+
+	packet = privateKey + "_" + DHKE1_num + "_" + sessionKey
+	return packet
+
+def main():
+	IP = socket.gethostbyname(socket.gethostname())
+	src_node = srcNode(IP, 1513)
+	src_node.start()
+
+	IPs = localAddresses()
+
+	for IP in IPs:
+		try:
+			src_node.connect_with_node(IP, 1513)
+
+			print("Connected to node.")
+			src_node.send_to_nodes({"DiscoveryPacket": discoveryPacket()})
+		except:
+			continue
+
+	src_node.stop()
+	print("Closing gemcoin.")
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit()
---
+++ gemcoin/peers/nodeconnection.py
@@ -0,0 +1,119 @@
+import socket
+import time
+import threading
+import json
+
+class NodeConnection(threading.Thread):
+    def __init__(self, main_node, sock, id, host, port):
+        super(NodeConnection, self).__init__()
+
+        self.host = host
+        self.port = port
+        self.main_node = main_node
+        self.sock = sock
+        self.terminate_flag = threading.Event()
+
+        self.id = str(id) # Make sure the ID is a string
+
+        self.EOT_CHAR = 0x04.to_bytes(1, 'big')
+
+        self.info = {}
+
+        self.sock.settimeout(10.0)
+
+        self.main_node.debug_print("NodeConnection.send: Started with client (" + self.id + ") '" + self.host + ":" + str(self.port) + "'")
+
+    def send(self, data, encoding_type='utf-8'):
+        if isinstance(data, str):
+            try:
+                self.sock.sendall( data.encode(encoding_type) + self.EOT_CHAR )
+
+            except Exception as e: # Fixed issue #19: When sending is corrupted, close the connection
+                self.main_node.debug_print("nodeconnection send: Error sending data to node: " + str(e))
+                self.stop() # Stopping node due to failure
+
+        elif isinstance(data, dict):
+            try:
+                json_data = json.dumps(data)
+                json_data = json_data.encode(encoding_type) + self.EOT_CHAR
+                self.sock.sendall(json_data)
+                
+            except TypeError as type_error:
+                self.main_node.debug_print('This dict is invalid')
+                self.main_node.debug_print(type_error)
+
+            except Exception as e: # Fixed issue #19: When sending is corrupted, close the connection
+                self.main_node.debug_print("nodeconnection send: Error sending data to node: " + str(e))
+                self.stop() # Stopping node due to failure
+
+        elif isinstance(data, bytes):
+            bin_data = data + self.EOT_CHAR
+            self.sock.sendall(bin_data)
+
+        else:
+            self.main_node.debug_print('datatype used is not valid plese use str, dict (will be send as json) or bytes')
+
+    def stop(self):
+        self.terminate_flag.set()
+
+    def parse_packet(self, packet):
+        try:
+            packet_decoded = packet.decode('utf-8')
+
+            try:
+                return json.loads(packet_decoded)
+
+            except json.decoder.JSONDecodeError:
+                return packet_decoded
+
+        except UnicodeDecodeError:
+            return packet
+
+    def run(self):
+        buffer = b'' # Hold the stream that comes in!
+
+        while not self.terminate_flag.is_set():
+            chunk = b''
+
+            try:
+                chunk = self.sock.recv(4096) 
+
+            except socket.timeout:
+                self.main_node.debug_print("NodeConnection: timeout")
+
+            except Exception as e:
+                self.terminate_flag.set() # Exception occurred terminating the connection
+                self.main_node.debug_print('Unexpected error')
+                self.main_node.debug_print(e)
+
+            if chunk != b'':
+                buffer += chunk
+                eot_pos = buffer.find(self.EOT_CHAR)
+
+                while eot_pos > 0:
+                    packet = buffer[:eot_pos]
+                    buffer = buffer[eot_pos + 1:]
+
+                    self.main_node.message_count_recv += 1
+                    self.main_node.node_message( self, self.parse_packet(packet) )
+
+                    eot_pos = buffer.find(self.EOT_CHAR)
+
+            time.sleep(0.01)
+
+        self.sock.settimeout(None)
+        self.sock.close()
+        self.main_node.node_disconnected( self ) # Fixed issue #19: Send to main_node when a node is disconnected. We do not know whether it is inbounc or outbound.
+        self.main_node.debug_print("NodeConnection: Stopped")
+
+    def set_info(self, key, value):
+        self.info[key] = value
+
+    def get_info(self, key):
+        return self.info[key]
+
+    def __str__(self):
+        return 'NodeConnection: {}:{} <-> {}:{} ({})'.format(self.main_node.host, self.main_node.port, self.host, self.port, self.id)
+
+    def __repr__(self):
+        return '<NodeConnection: Node {}:{} <-> Connection {}:{}>'.format(self.main_node.host, self.main_node.port, self.host, self.port)
