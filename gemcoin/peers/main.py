06ea0a7d61276de3d7ae259d1f7b16dd16fb978e
Vlad Usatii <vladusatii@gmail.com>
2021-12-21 20:47:51 -0600

    added if

--- gemcoin/peers/main.py
+++ gemcoin/peers/main.py
@@ -5,13 +5,43 @@ import time
 import asyncio
 import random
 
-from p2pLib import srcNode
+from node import Node
 
 IP = socket.gethostbyname(socket.gethostname())
 
 # diffie-hellman constants
 DH_GEN = 9
 DH_MOD = 37
+
+
+# Network event handler
+class srcNode(Node):
+	def __init__(self, host, port, id=None, callback=None, max_connections=0):
+		super(srcNode, self).__init__(host, port, id, callback, max_connections)
+		print("gemcoin searching for peers. . .")
+
+	def outbound_node_connected(self, node):
+		print("Connected to potential peer.")
+        
+	def inbound_node_connected(self, node):
+		print("inbound_node_connected: (" + self.id + "): " + node.id)
+		return True
+
+	def inbound_node_disconnected(self, node):
+		print("(Inbound) Disconnected from peer.")
+
+	def outbound_node_disconnected(self, node):
+		print("(Outbound) Disconnected from peer.")
+
+	def node_message(self, node, data):
+		print("node_message (" + self.id + ") from " + node.id + ": " + str(data))
+        
+	def node_disconnect_with_outbound_node(self, node):
+		print("node wants to disconnect with oher outbound node: (" + self.id + "): " + node.id)
+        
+	def node_request_to_stop(self):
+		print("node is requested to stop (" + self.id + "): ")
+
 
 def localAddresses():
 	c1, c2 = '(', ')'
@@ -51,9 +81,12 @@ def main():
 
 	for IP in IPs:
 		try:
-			src_node.connect_with_node(IP, 1513)
+			outbound = src_node.connect_with_node(IP, 1513)
 			time.sleep(1)
-			src_node.send_to_nodes({"message": discoveryPacket()})
+
+			if outbound:
+				src_node.send_to_nodes({"message": discoveryPacket()})
+				print("Sent key.")
 		except:
 			continue
 
