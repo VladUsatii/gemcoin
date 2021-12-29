06310c84468ea7b1244333188362df9617a8e6df
Vlad Usatii <vladusatii@gmail.com>
2021-12-29 00:30:59 -0600

    installation procedure in bash

---
+++ requirements.md
@@ -0,0 +1,9 @@
+# Other unmentioned requirements
+
+### LevelDB
+
+Python uses LevelDB to access plyvel, the database wrapper. If you don't have it, it is crucial that you install it via brew in order to run the chain.
+
+```
+brew install leveldb
+```
--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+# installs all dependencies
+pip install -r requirements.txt;
 
+# remember to execute setup.sh if you change location of gemcoin
+chosen_path="$PWD";
+echo "alias gemcoin=\'python3 $chosen_path/peers/main.py\'" >> ~/.bash_profile;
