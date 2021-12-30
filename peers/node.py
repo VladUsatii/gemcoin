cb8c53241ccdcc5f515bbb0b649127855e492026
Vlad Usatii <vladusatii@gmail.com>
2021-12-29 19:30:03 -0600

    will fix this later

--- peers/node.py
+++ peers/node.py
@@ -119,7 +119,7 @@ class Node(threading.Thread):
 
 			# payload = rlp_encode(self.id)
 			# sock.send(payload.encode('utf-8'))
-			sock.send(self.id.encode('utf-8'))
+			sock.send(self.id[0].encode('utf-8'))
 
 			connected_node_id = sock.recv(4096).decode('utf-8')
 			# connected_node_id = rlp_decode(connected_node_id)
